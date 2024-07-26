3ae2e8decd7834b8a174b2dc889771ffab8bcdc5
cjbruin23 <colinjb23@gmail.com>
2024-07-26 15:15:17 -0700

    adding in a form for user input

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,40 @@
+import axios from 'axios';
 import './App.css'
+
+type UserInformation = {
+  email: string;
+  username: string;
+  password: string;
+}
 
 function App() {
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const handleSubmit = (event: any) => {
     event.preventDefault();
+    const formData = new FormData(event.target);
+    const email = formData.get('inputEmail')!.toString();
+    const username = formData.get('inputUsername')!.toString();
+    const password = formData.get('inputPassword')!.toString();
+
+
+    const userInformation = {
+      email,
+      username,
+      password
+    }
+
+    callApi(userInformation)
+    
+  }
+
+  const callApi = async (userInformation: UserInformation) => {
+    console.log('userInformation', userInformation);
+    axios.post('http://localhost:8080/player', userInformation).then((res) => {
+      console.log('res', res)
+    }).catch((err) => console.log('err', err))
   }
   
-
   return (
     <>
     <h1 className="text-3xl font-bold underline">
@@ -22,6 +49,8 @@ function App() {
 
         <label>Password:</label>
         <input type="text" name="inputPassword" />
+
+        <button type="submit">Login</button>
       </form>
     </>
   )
