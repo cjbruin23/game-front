0938c3174b1e39d4e15b0d247202b81acf3760a8
cjbruin23 <colinjb23@gmail.com>
2024-07-27 09:47:04 -0700

    deciding whether button pressed is login or signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,77 @@
-import axios from 'axios';
-import './App.css'
+import axios from "axios";
+import "./App.css";
 
 type UserInformation = {
   email: string;
   username: string;
   password: string;
-}
+};
 
 function App() {
-
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const handleSubmit = (event: any) => {
     event.preventDefault();
+    const buttonPressed = event.nativeEvent.submitter.innerText;
     const formData = new FormData(event.target);
-    const email = formData.get('inputEmail')!.toString();
-    const username = formData.get('inputUsername')!.toString();
-    const password = formData.get('inputPassword')!.toString();
-
+    const email = formData.get("inputEmail")!.toString();
+    const username = formData.get("inputUsername")!.toString();
+    const password = formData.get("inputPassword")!.toString();
 
     const userInformation = {
       email,
       username,
-      password
+      password,
+    };
+
+    callApi(userInformation, buttonPressed);
+  };
+
+  const callApi = async (userInformation: UserInformation, action: string) => {
+    console.log("userInformation", userInformation);
+    const payload = {
+      userInformation,
+      action
     }
+    axios
+      .post("http://localhost:8080/player", payload)
+      .then((res) => {
+        console.log("res", res);
+      })
+      .catch((err) => console.log("err", err));
+  };
 
-    callApi(userInformation)
-    
-  }
-
-  const callApi = async (userInformation: UserInformation) => {
-    console.log('userInformation', userInformation);
-    axios.post('http://localhost:8080/player', userInformation).then((res) => {
-      console.log('res', res)
-    }).catch((err) => console.log('err', err))
-  }
-  
   return (
     <>
-    {/* <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-      <form onSubmit={handleSubmit}>
-        <label>Email:</label>
-        <input type="text" name="inputEmail"/>
-        
-        <label>Username:</label>
-        <input type="text" name="inputUsername"/>
+      <div id="header">
+        <h1>
+          <strong>Welcome to Conquest!</strong>
+        </h1>
 
-        <label>Password:</label>
-        <input type="text" name="inputPassword" />
+        <form onSubmit={handleSubmit}>
+          <label>Email:</label>
+          <input type="text" name="inputEmail" className="border-2 mb-4" />
+          <br />
 
-        <button type="submit">Login</button>
-      </form> */}
+          <label>Username:</label>
+          <input type="text" name="inputUsername" className="border-2 mb-4" />
+          <br />
+
+          <label>Password:</label>
+          <input type="text" name="inputPassword" className="border-2 mb-4" />
+          <br />
+
+          <button className="group relative inline-flex h-12 items-center justify-center overflow-hidden rounded-md bg-neutral-950 px-6 font-medium text-neutral-200">
+            <span>Login</span>
+            <div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-12deg)_translateX(-100%)] group-hover:duration-1000 group-hover:[transform:skew(-12deg)_translateX(100%)]">
+              <div className="relative h-full w-8 bg-white/20"></div>
+            </div>
+          </button>
+          <button type="submit">Login</button>
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
