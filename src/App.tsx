e879c6b1a2160b7ac5b69dc61915b6e501ccbe5b
cjbruin23 <colinjb23@gmail.com>
2024-10-24 15:22:07 -0700

    adding in axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
 import "@/App.css";
 import Home from "@components/Home";
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [userName, setUserName] = useState("")
-  
+  const [userName, setUserName] = useState("");
+
   function handleNameFormSubmission(username: string) {
-    console.log('data', username)
-    setUserName(username)
+    console.log("data", username);
+    setUserName(username);
   }
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5164/weatherforecast")
+      .then((res) => console.log("res", res))
+      .catch((err) => console.log("err", err));
+  }, []);
 
   return (
     <>
-    <Home submitForm={handleNameFormSubmission}/>
+      <Home submitForm={handleNameFormSubmission} />
 
-    <div>{userName ? `Welcome ${userName}` : ""}</div>
-
+      <div>{userName ? `Welcome ${userName}` : ""}</div>
     </>
   );
 }
