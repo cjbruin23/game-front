bb0ab2cb2c3b9b3fc8c6078c29d6ac9387a91a17
cjbruin23 <colinjb23@gmail.com>
2024-10-24 14:23:07 -0700

    adding

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(inputValue)
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          What is your name?
+          <input type="text" value={inputValue} onChange={handleChange}></input>
+        </label>
+        <br></br>
+        <button type="submit">Submit</button>
+      </form>
+
+      <div>
+        {(inputValue) ? `Welcome ${inputValue}` : ''}
+      </div>
+    </>
+  );
+}
+
+export default App;
